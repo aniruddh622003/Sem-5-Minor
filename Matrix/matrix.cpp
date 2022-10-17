31d325987a458cce703752316df35fb89599301c
Aniruddh Dev Upadhyay <aniruddh622003@gmail.com>
2022-10-17 14:01:16 +0530

    Matrix addition

--- Matrix/matrix.cpp
+++ Matrix/matrix.cpp
@@ -8,8 +8,10 @@ int main()
     int m, n;
     std::cin >> m >> n;
     std::vector<std::vector<int>> mat1 = input_matrix(m, n);
-    // std::vector<std::vector<int>> mat2 = input_matrix(m, n);
-    // int *res = add_matrix(m, n, (int *)mat1, (int *)mat2);
+    std::vector<std::vector<int>> mat2 = input_matrix(m, n);
+    std::vector<std::vector<int>> res = add_matrix(m, n, mat1, mat2);
     print_matrix(m, n, mat1);
+    print_matrix(m, n, mat2);
+    print_matrix(m, n, res);
     return 0;
 }
--- Matrix/matrix.hpp
+++ Matrix/matrix.hpp
@@ -37,15 +37,24 @@ void print_matrix(int m, int n, std::vector<std::vector<int>> mat)
     }
 }
 
-// int *add_matrix(int m, int n, int *mat1, int *mat2)
-// {
-//     int result[m][n];
-//     for (int i = 0; i < m; i++)
-//     {
-//         for (int j = 0; j < n; j++)
-//         {
-//             result[i][j] = *((mat1 + i * n) + j) + *((mat2 + i * n) + j);
-//         }
-//     }
-//     return (int *)result;
-// }
+int get_element_at_index(std::vector<std::vector<int>> mat, int row, int col)
+{
+    return mat[row - 1][col - 1];
+}
+
+std::vector<std::vector<int>> add_matrix(int m, int n, std::vector<std::vector<int>> mat1, std::vector<std::vector<int>> mat2)
+{
+    int val;
+    std::vector<std::vector<int>> mat;
+    for (int i = 0; i < m; i++)
+    {
+        std::vector<int> row;
+        for (int j = 0; j < n; j++)
+        {
+            val = mat1[i][j] + mat2[i][j];
+            row.push_back(val);
+        }
+        mat.push_back(row);
+    }
+    return mat;
+}
